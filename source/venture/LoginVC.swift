b64c6bc77f07737b0338eaca029852e71ce763bc
j-chao <justin_chao@utexas.edu>
2017-03-23 18:17:58 -0500

    added FB login capability

--- source/venture/LoginVC.swift
+++ source/venture/LoginVC.swift
@@ -8,8 +8,9 @@
 
 import UIKit
 import Firebase
+import FBSDKLoginKit
 
-class LoginVC: UIViewController, UITextFieldDelegate {
+class LoginVC: UIViewController, UITextFieldDelegate, FBSDKLoginButtonDelegate {
     
     @IBOutlet weak var email: UITextField!
     @IBOutlet weak var password: UITextField!
@@ -22,10 +23,55 @@ class LoginVC: UIViewController, UITextFieldDelegate {
         email.delegate = self
         password.delegate = self
         
+        let fbLoginButton = FBSDKLoginButton()
+        view.addSubview(fbLoginButton)
+        fbLoginButton.frame = CGRect(x: 16, y: 50, width: view.frame.width - 32, height: 50)
+        
+        fbLoginButton.delegate = self
+
+    }
+    
+    func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
+        print ("Did log out of facebook")
     }
    
+    func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!) {
+        if error != nil {
+            print (error)
+            return
+        }
+        
+        let credential = FIRFacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+        
+        FIRAuth.auth()?.signIn(with: credential, completion: {
+            user, error in
+            if error != nil {
+                print ("Incorrect email/password")
+                self.errorMessage.text = "Incorrect email and/or password"
+            }
+            else {
+                print ("User logged in with Facebook !")
+                
+                FIRAuth.auth()?.currentUser!.link(with: credential) { (user, error) in
+                    if user != nil && error == nil {
+                        return
+                    }
+                }
+                
+                let storyboard: UIStoryboard = UIStoryboard(name: "trip", bundle: nil)
+                let vc = storyboard.instantiateViewController(withIdentifier: "tripNavCtrl")
+                self.show(vc, sender: self)
+            }
+        })
+        
+        
+    }
+    
     @IBAction func login(_ sender: Any) {
-        FIRAuth.auth()?.signIn(withEmail: email.text!, password: password.text!, completion: {
+        
+    let credential = FIREmailPasswordAuthProvider.credential(withEmail: email.text!, password: password.text!)
+        
+        FIRAuth.auth()?.signIn(with: credential, completion: {
             user, error in
             if error != nil {
                 print ("Incorrect email/password")
