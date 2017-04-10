979c16f2bdad955c7f80a25f2879cc7fe0f0367b
Connie Liu <cliu42@nga.cs.utexas.edu>
2017-04-10 17:06:12 -0500

    added food storyboards and swift files

---
+++ source/venture/foodCell.swift
@@ -0,0 +1,30 @@
+//
+//  foodCell.swift
+//  venture
+//
+//  Created by Connie Liu on 4/10/17.
+//  Copyright © 2017 Group1. All rights reserved.
+//
+
+import UIKit
+
+class foodCell: UITableViewCell {
+
+
+    @IBOutlet weak var foodNameLbl: UILabel!
+    @IBOutlet weak var foodCategoryLbl: UILabel!
+    @IBOutlet weak var foodRatingLbl: UILabel!
+    @IBOutlet weak var foodDistanceLbl: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
