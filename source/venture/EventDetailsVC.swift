c2b3647dac2577f04d3762545bd63817beb99381
j-chao <justin_chao@utexas.edu>
2017-04-12 18:12:42 -0500

    added eventLoc to event details view

--- source/venture/EventDetailsVC.swift
+++ source/venture/EventDetailsVC.swift
@@ -14,6 +14,8 @@ class EventDetailsVC: UIViewController {
     
     var ref:FIRDatabaseReference?
     let userID = FIRAuth.auth()?.currentUser?.uid
+    
+    let myRequest = DispatchGroup()
   
     var date:String!
     var event:String!
@@ -21,9 +23,13 @@ class EventDetailsVC: UIViewController {
     @IBOutlet weak var eventTime: UILabel!
     @IBOutlet weak var eventDate: UILabel!
     @IBOutlet weak var eventDesc: UILabel!
+    @IBOutlet weak var eventLoc: UILabel!
+    
     @IBOutlet weak var map: MKMapView!
     
     override func viewDidLoad() {
+        myRequest.enter()
+        
         self.setBackground()
         super.viewDidLoad()
         
@@ -33,6 +39,7 @@ class EventDetailsVC: UIViewController {
             if snapshot.exists() {
                 let desc = (snapshot.value as? NSDictionary)?["eventDesc"]
                 let timeRec = (snapshot.value as? NSDictionary)?["eventTime"]
+                let loc = (snapshot.value as? NSDictionary)?["eventLoc"] as! String!
               
                 let timeDate = timeFromStringTime(timeStr: timeRec as! String)
                 let timeDisplay = stringTimefromDate(date: timeDate)
@@ -40,11 +47,35 @@ class EventDetailsVC: UIViewController {
                 self.eventDate.text = self.date
                 self.eventTime.text = timeDisplay as String?
                 self.eventDesc.text = desc as! String?
+                self.eventLoc.text = loc
             }
+        })
+        myRequest.leave()
+        
+        myRequest.notify(queue: DispatchQueue.main, execute: {
+            self.setMapMark()
         })
     }
     
-    override func viewWillAppear(_ animated: Bool) {
+    func setMapMark() {
+        let address = self.eventLoc.text
+        let geocoder = CLGeocoder()
+        geocoder.geocodeAddressString(address!) { [weak self] placemarks, error in
+            if let placemark = placemarks?.first, let location = placemark.location {
+                let mark = MKPlacemark(placemark: placemark)
+                
+                if var region = self?.map.region {
+                    region.center = location.coordinate
+                    region.span.longitudeDelta = 0.1
+                    region.span.latitudeDelta = 0.1
+                    self?.map.setRegion(region, animated: true)
+                    self?.map.addAnnotation(mark)
+                }
+            }
+        }
+    }
+    
+    func setMapCoor() {
         let span:MKCoordinateSpan = MKCoordinateSpanMake(0.1, 0.1)
         let location:CLLocationCoordinate2D = CLLocationCoordinate2DMake(30.286114, -97.739347)
         let region:MKCoordinateRegion = MKCoordinateRegionMake(location, span)
@@ -54,23 +85,6 @@ class EventDetailsVC: UIViewController {
         annotation.coordinate = location
         annotation.title = "HOOK 'EM"
         map.addAnnotation(annotation)
-        
-        
-        let address = "714 W 22nd St, Austin, 78705"
-        let geocoder = CLGeocoder()
-        geocoder.geocodeAddressString(address) { [weak self] placemarks, error in
-            if let placemark = placemarks?.first, let location = placemark.location {
-                let mark = MKPlacemark(placemark: placemark)
-                
-                if var region = self?.map.region {
-                    region.center = location.coordinate
-                    region.span.longitudeDelta /= 8.0
-                    region.span.latitudeDelta /= 8.0
-                    self?.map.setRegion(region, animated: true)
-                    self?.map.addAnnotation(mark)
-                }
-            }
-        }
     }
     
     
